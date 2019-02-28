df387f0d532ceff64aaf003eca806759eea4389b
David Dinata <ddinata@ucsd.edu>
2019-02-28 13:08:11 -0800

    thirdparty lab api

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
 	// add your code here
+    L.mapquest.key = 'wtz9M8Bfit0FlZAnkX8UNs0GpXGdecNQ';
+    var map = L.mapquest.map('map', {
+        center: [32.8788, -117.2359],
+        layers: L.mapquest.tileLayer('map'),
+        zoom: 12,
+        zoomContrl: false
+    });
+
+    L.marker([32.8788, -117.2359]).addTo(map);
 }
